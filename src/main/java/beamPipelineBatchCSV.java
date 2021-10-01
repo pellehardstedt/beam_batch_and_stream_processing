fcfdbfe9f3d63f09cdb7682514b0769a1d65e05d
pellehardstedt <pelle.hardstedt@gmail.com>
2021-10-01 10:48:47 +0200

    two main methods

---
+++ src/main/java/beamPipelineBatchCSV.java
@@ -0,0 +1,36 @@
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.ParDo;
+
+public class beamPipelineBatchCSV {
+
+    static class removeEan extends DoFn<String, String> {
+        @ProcessElement
+        public void processElement(@Element String element, OutputReceiver<String> receiver ){
+            String[] columns = element.split(",");
+            String stringOut = "";
+            for (int i = 1; i < columns.length; i++) {
+                stringOut = stringOut.concat(columns[i]);
+            }
+            receiver.output(stringOut);
+        }
+    }
+
+    public static void main(String[] args){
+        PipelineOptions options = PipelineOptionsFactory.create();
+
+        Pipeline p = Pipeline.create();
+        p.apply("read csv", TextIO.read().from("C:\\Users\\Pelle\\Desktop\\productsPipeline\\src\\main\\resources\\upc_corpus - Copy.csv"))
+                .apply(ParDo.of(new removeEan()))
+                .apply("write csv", TextIO.write().to("C:\\Users\\Pelle\\Desktop\\productsPipeline\\src\\main\\resources\\upc_corpus_new.csv"));
+        try {
+            p.run().waitUntilFinish();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/main/java/beamPipelineKafkaStream.java
@@ -0,0 +1,37 @@
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.io.kafka.KafkaIO;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.Values;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+
+public class beamPipelineKafkaStream {
+
+    static class oneLog extends DoFn<String, String> {
+        @ProcessElement
+        public void processElement(@Element String element, OutputReceiver<String> receiver ){
+            System.out.println(element);
+            receiver.output(element);
+        }
+    }
+
+    public static void main(String[] args){
+        PipelineOptions options = PipelineOptionsFactory.create();
+
+        Pipeline p = Pipeline.create();
+
+        PCollection<String> message = p.apply(KafkaIO.<String,String>read()
+                .withBootstrapServers("localhost:9092,localhost:9093,localhost:9094")
+                .withTopic("rsvp")
+                .withKeyDeserializer(StringDeserializer.class)
+                .withValueDeserializer(StringDeserializer.class)
+                .withoutMetadata())
+                .apply(Values.<String>create())
+                .apply(ParDo.of(new oneLog()));
+        p.run().waitUntilFinish();
+    }
+}
